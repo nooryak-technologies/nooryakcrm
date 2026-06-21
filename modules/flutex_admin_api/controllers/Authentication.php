c9ff19c3e2ee2c15c24e253fbf890db096fe045e
samirahamath <samir52790@gmail.com>
2026-06-21 13:38:09 +0530

    temp debug login diagnostics

--- modules/flutex_admin_api/controllers/Authentication.php
+++ modules/flutex_admin_api/controllers/Authentication.php
@@ -49,12 +49,20 @@ class Authentication extends RestController
         try {
             $this->load->model('Authentication_model');
 
+            $this->load->database();
+            $debug_info = [
+                'db' => $this->db->database,
+                'email' => $postData['email'],
+                'password_length' => strlen($postData['password']),
+                'has_user' => $this->db->where('email', $postData['email'])->get(db_prefix().'staff')->num_rows()
+            ];
+
             $success = $this->Authentication_model->login($postData['email'], $postData['password'], true, true);
     
             if (is_array($success) && isset($success['memberinactive'])) {
-                $this->response(['message' => _l('admin_auth_inactive_account')], RestController::HTTP_FORBIDDEN);
+                $this->response(['message' => _l('admin_auth_inactive_account'), 'debug' => $debug_info], RestController::HTTP_FORBIDDEN);
             } elseif (false == $success) {
-                $this->response(['message' => _l('admin_auth_invalid_email_or_password')], RestController::HTTP_UNAUTHORIZED);
+                $this->response(['message' => _l('admin_auth_invalid_email_or_password'), 'debug' => $debug_info], RestController::HTTP_UNAUTHORIZED);
             }
     
             $table = db_prefix().'staff';
