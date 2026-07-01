99d953986974997792735b6f9f0f3848f5553296
samirahamath <samir52790@gmail.com>
2026-07-01 16:06:04 +0530

    dompdf

--- application/views/themes/perfex/views/my_invoicepdf.php
+++ application/views/themes/perfex/views/my_invoicepdf.php
@@ -55,10 +55,8 @@ if ($note_text === '') {
 }
 
 // Helper: format money
-if (!function_exists('_inv_money')) {
-    function _inv_money($amount, $symbol) {
-        return $symbol . number_format((float)$amount, 2, '.', ',');
-    }
+function _inv_money($amount, $symbol) {
+    return $symbol . number_format((float)$amount, 2, '.', ',');
 }
 
 // Logo
@@ -409,17 +407,13 @@ $html = '
             border-top: 1px solid #dcecee;
         }
         
-        .footer-bar {
+        .footer-table {
             width: 100%;
             margin-top: 40px;
             background-color: #17a2b8;
+            border-collapse: collapse;
             border-radius: 12px;
             overflow: hidden;
-        }
-        
-        .footer-table {
-            width: 100%;
-            border-collapse: collapse;
         }
         
         .footer-table td {
@@ -559,21 +553,19 @@ $html = '
     </table>
 
     <!-- FOOTER -->
-    <div class="footer-bar">
-        <table class="footer-table">
-            <tr>
-                <td style="width: 33.33%; text-align: left;">
-                    ' . ($company_website !== '' ? htmlspecialchars($company_website) : '') . '
-                </td>
-                <td style="width: 33.33%; text-align: center;">
-                    ' . ($company_phone !== '' ? htmlspecialchars($company_phone) : '') . '
-                </td>
-                <td style="width: 33.33%; text-align: right;">
-                    ' . ($company_email !== '' ? htmlspecialchars($company_email) : '') . '
-                </td>
-            </tr>
-        </table>
-    </div>
+    <table class="footer-table">
+        <tr>
+            <td style="width: 33.33%; text-align: left;">
+                ' . ($company_website !== '' ? htmlspecialchars($company_website) : '') . '
+            </td>
+            <td style="width: 33.33%; text-align: center;">
+                ' . ($company_phone !== '' ? htmlspecialchars($company_phone) : '') . '
+            </td>
+            <td style="width: 33.33%; text-align: right;">
+                ' . ($company_email !== '' ? htmlspecialchars($company_email) : '') . '
+            </td>
+        </tr>
+    </table>
 
 </div>
 
